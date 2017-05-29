44e8070dd8be1bcd22725efdd509af48e656c745
andrius <a.kaliacius@gmail.com>
2017-05-29 19:48:45 +0100

    adding 10 b

--- year2015/Q10_2015_B.java
+++ year2015/Q10_2015_B.java
@@ -3,13 +3,20 @@ package year2015;
 public class Q10_2015_B{
 
     public static void main(String[] args){
-        int[] r = {1, 2, 5, 5, 3, 1, 2, 4, 3, 2, 2, 2, 2, 3, 6, 5, 5, 3, 1, 1, 1, 1, 1, 1, 1, 1};
-        int[] m = {1, 2, 5, 5};
-
+        int[] r = {1, 2, 5, 5, 3, 1, 2, 4, 3, 2, 2, 2, 2, 3, 6, 5, 5, 3, 1};
         System.out.println(getLongestRunLength(r));
     }
 
     public static int getLongestRunLength(int[] v){
-        return 0;
+        int max = 0;
+        for(int i = 0; i < v.length; i++){
+            int counter = 1;
+            for(int j = i+1; j < v.length; j++){
+                if(v[i] != v[j]) break;
+                counter++;
+            }
+            if(counter > max) max = counter;
+        }
+        return max;
     }
 }
